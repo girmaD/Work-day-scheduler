f87e90a233cabc61c7c13f90c0d3e490b85763d3
Girma Ebssa <girma.ebssa@gmail.com>
2020-10-04 01:00:01 -0700

    adding functionality to the script file

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
 let timeNow = moment();
+let scheduleArr = [];
+let plan;
+let storedArr;
+let retrievedSchedule;
 
 $('#currentDay').text(timeNow.format("dddd, MMMM Do"));
 let currentTime = timeNow.format("H");
-// console.log(timeNow)
-console.log(currentTime)
-// for each elemenet contaning form-control class
-$(".form-control").each(function(form){
-    // if current time equals the data number in the element
-    if($(this).data("number") == currentTime){
-        // add a "present" class to the element
-        $(this).addClass("present");
-        console.log(currentTime  +"=="  + $(this).data("number"))
+
+function renderSchedule(){
+    storedArr = JSON.parse(localStorage.getItem("mySchedule"));
+    if(storedArr !== "null"){
+        storedArr.forEach(function(arr){
+            console.log(arr)
+            retrievedSchedule = arr;
+            text = retrievedSchedule.text;
+            if(text !== "null"){                
+                $(this).parent("div").children(".inputText").children("textarea").val(text);
+            }
+        })
     }
-    // if current time greater the data number in the element
-    else if($(this).data("number") < currentTime){
-        // add a "past" class to the element
-        $(this).addClass("past");
-        console.log(currentTime  +">"  + $(this).data("number"))
+}
+
+// for each element contaning form-control class
+function coloredDiv(){
+    $(".form-control").each(function(form){
+        // if current time equals the data number in the element
+        if($(this).data("number") == currentTime){
+            // add a "present" class to the element
+            $(this).addClass("present");
+            // console.log(currentTime  +"=="  + $(this).data("number"))
+        }
+        // if current time greater the data number in the element
+        else if($(this).data("number") < currentTime){
+            // add a "past" class to the element
+            $(this).addClass("past");
+            // console.log(currentTime  +">"  + $(this).data("number"))
+        }
+        else {
+            // add a "future" class to the element
+            $(this).addClass("future");
+            // console.log(currentTime  +"<"  + $(this).data("number"))
+        }    
+    })
+}        
+    
+$(".saveBtn").on("click", function(event){
+    event.preventDefault();
+    let text = $(this).parent("div").children(".inputText").children("textarea").val();
+    let time = $(this).parent("div").children(".time-block").text();
+    
+    if(text === ""){
+        alert("you have to write on the textarea to save it")
+        return
     }
     else {
-        // add a "future" class to the element
-        $(this).addClass("future");
-        console.log(currentTime  +"<"  + $(this).data("number"))
+        plan = {time: time, text: text};         
     }    
+       
+    scheduleArr = JSON.parse(localStorage.getItem("mySchedule"));
+    if(!scheduleArr){        
+        localStorage.setItem("mySchedule", JSON.stringify([plan]));        
+    }    
+    else {
+        scheduleArr.push(plan);
+        localStorage.setItem("mySchedule", JSON.stringify(scheduleArr));
+    }    
+    // $(this).parent("div").children(".inputText").children("textarea").replaceWith(('<textarea class="form-control">' + text + '</textarea>'));
 })
-$(".saveBtn").on("click", function(){
-    let text = $("textarea").val();
-    console.log($("textarea").val());
-})
+
+
+coloredDiv();
+   
+     
+    
